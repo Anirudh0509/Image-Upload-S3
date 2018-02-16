256d51fbc282ab2f1f9aa1e10fad4db08ba0a5bd
Anirudh Aggarwal <aniragga@in.ibm.com>
2018-02-17 03:36:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var app = express();
+var multer = require('multer'); // extract the image from the POST request body
+var AWS = require('aws-sdk'); // The AWS-SDK includes the API of the AWS components
+var fs = require('fs'); // read files from the disk
+var bodyParser= require('body-parser');
+
+AWS.config.update({
+  region: "us-east-2",
+   accessKeyID: 'AKIAJUZAYCBIAOY65SIQ',
+   secretAccessKey: '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',
+});
+
+app.get('/upload/image', function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+app.use(bodyParser.text());
+
+var photoBucket = new AWS.S3({params: {Bucket: 'elasticbeanstalk-us-east-2-205654503547'}});
+var upload = multer().single('file');
+
+app.post('/upload/image',upload, function(req,res){
+
+  //'key' is the file path. In S3 bucket everything is a key-value pair,
+  //So in order to keep the file under the 'uploads' folder, we need the 
+  //'uploads/' prefix to this key.
+  var key = 'uploads/' + req.file.originalname.toString();
+
+  //Upload to the S3 bucket via AWS-SDK.
+  photoBucket.upload({
+        ACL: 'public-read',    //File has public read access.
+        Body: req.file.buffer, //The image itself.
+        Key: key, //File path
+        ContentType: 'application/octet-stream'
+     }).send(function(err,data) {
+           if(err) {
+              return res.end(err);
+           }
+           res.end("Fileuploaded to S3!");
+  });
+});
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log('Example app listening on port 3000!');
+});
